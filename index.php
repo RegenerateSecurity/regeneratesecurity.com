f87fc85db1adf8f4b07bd9e857984b3e8864cb09
holly <holly@localhost.localdomain>
2019-05-20 16:40:16 -0400

    Building up theme

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 <?php
 $payload = array('ping' => 'hello');
 $response = postJson('hello', '1.0', json_encode($payload));
+print $response;
 ?>
 </div>
 </div>
