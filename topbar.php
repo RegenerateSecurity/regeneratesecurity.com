adb6866e4e65e28863b78fb2bc39defb6db37af9
holly <holly@localhost.localdomain>
2019-05-20 12:06:24 -0400

    Building up theme

--- topbar.php
+++ topbar.php
@@ -1,17 +1,9 @@
 <?php
 function drawTopBar() {
-  // Inefficient to define var that might not be used, but the benefit is readability
-  $adminLink    = '<a href="/admin/index.php">Admin</a>';
-  $profileLink  = '<a href="/profile.php">My Profile</a>';
-  $logoutLink   = '<a href="/logout.php">Log out</a>';
-  $loginLink    = '<a href="/login.php">Sign in</a>';
-  $registerLink = '<a href="/register.php">Register</a>';
-  //if      ($_SESSION['privs'] == 3) { print $adminLink   . ' | ' . $profileLink . ' | ' . $logoutLink; } // user is admin
-  //else if ($_SESSION['privs'] >= 0) { print $profileLink . ' | ' . $logoutLink; }			 // user is user
-  //else                              { print $loginLink   . ' | ' . $registerLink; }			 // user is unauthed
-  print $loginLink   . ' | ' . $registerLink
+  print '<a href="/login.php">Sign in</a>' . | . '<a href="/register.php">Register</a>';
 }
 ?>
+
 <div class="top-bar">
   <div class="logo"><a href="/">RegenSec</a></div>
   <div><?php drawTopBar(); ?></div>
