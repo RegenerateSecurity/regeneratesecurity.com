b74ec97592c3c3cd2f0e69fc58506db57c2f2031
holly <holly@localhost.localdomain>
2019-05-20 17:11:25 -0400

    Building up theme

--- head.php
+++ head.php
@@ -6,16 +6,13 @@
 </head>
 <body>
 <?php
+// Hit the API on each page request and check everything is okay
 $response = postJson('hello', '1.0', json_encode(array('ping' => 'hello')));
 $responseJson = json_decode($response, true);
 if ($responseJson === null or $responseJson['ping'] != "olleh") {
   print '<div class="api-status">Service Status: API ERROR</div>';
 }
-else {
-  print '<div class="api-status">Service Status: API GOOD</div>';
-}
 ?>
-<div class="api-status">
 <div class="cookie-law">
 <div class="cookie-text"><p><a href="/cookies.php">This site uses cookies; You can click here to change your cookie settings or learn more.</a></p></div>
 <div class="cookie-button"><input class="admin-button" type="button" value="Dismiss"></div></p>
