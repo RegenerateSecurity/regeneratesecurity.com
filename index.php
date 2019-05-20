00f0df0715d0fad525a632de80c940955db57eb9
holly <holly@localhost.localdomain>
2019-05-20 12:04:45 -0400

    Building up theme

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 ?>
-  <div class="cookie-bar">
-Cookies go here init
-  <div>
-  <div class="profile-bar">
-This one for if you're logged in
-  <div>
-  <div class="title-bar">
-Regenerate Security
-  <div>
-  <div class="nav-bar">
-News | Dashboard | Data | VulnDB
-  <div>
   <div class="content-wrapper">
 Content here
   <div>
 
-</body>
-</html>
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/footer.php';
+?>
---
+++ navbar.php
@@ -0,0 +1,9 @@
+<div><span class="title-left">Regenerate</span><span class="title-right">Security</span></div>
+<div class="nav-bar">
+<ul>
+<li>News</li>
+<li>Dashboard</li>
+<li>Status</li>
+<li>VulnDB</li>
+</ul>
+</div>
