06a30e64132b62680052bee9175a397acd00e78a
holly <holly@localhost.localdomain>
2019-05-20 12:23:11 -0400

    Building up theme

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
 ?>
-  <div class="content-wrapper">
+<div class="content-wrapper">
+<div class="content">
 Content here
-  <div>
+</div>
+</div>
 
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/footer.php';
