7e9f98c95be348642f2d32d6add3678a4af5942d
holly <holly@localhost.localdomain>
2019-05-20 11:40:26 -0400

    Building up theme

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-Initial upload
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/theme/main.css" />
+</head>
+<body>
+  <div class="cookie-bar">
+Cookies go here init
+  <div>
+  <div class="profile-bar">
+This one for if you're logged in
+  <div>
+  <div class="title-bar">
+Regenerate Security
+  <div>
+  <div class="nav-bar">
+News | Dashboard | Data | VulnDB
+  <div>
+  <div class="content-wrapper">
+Content here
+  <div>
+
+</body>
+</html>
