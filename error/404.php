282486e6f67c8b6da7c431f898dd28b455ecf69f
holly <holly@localhost.localdomain>
2019-05-20 15:01:42 -0400

    Building up theme

---
+++ error/404.php
@@ -0,0 +1,19 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
+?>
+<div class="content-wrapper">
+<div class="center-text">
+<h1 class="black-text">Error: File Not Found</h1>
+<p>Sorry :(</p>
+</div>
+<div class="content-block">
+Content here
+</div>
+</div>
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/footer.php';
+?>
