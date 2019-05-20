94c418d336a7f26d5c4ad84cc79e5ef391d3cc8f
holly <holly@localhost.localdomain>
2019-05-20 15:03:09 -0400

    Building up theme

---
+++ cookies.php
@@ -0,0 +1,19 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
+?>
+<div class="content-wrapper">
+<div class="center-text">
+<h1 class="black-text">Cookie Policy</h1>
+<p>Dur to EU law, cookies init</p>
+</div>
+<div class="content-block">
+Content here
+</div>
+</div>
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/footer.php';
+?>
