87134c4e96a71ee7c683205c2c43ca899a840a53
holly <holly@localhost.localdomain>
2019-05-23 07:16:33 -0400

    building login

--- profile.php
+++ profile.php
@@ -1,3 +1,10 @@
 <?php
-  print htmlspecialchars($_COOKIE['sessionToken']);
+$response = postJson('sessionCheck', '1.0', json_encode(array('token' => $_COOKIE['sessionToken'])));
+$responseJson = json_decode($response, true);
+if ($responseJson === null or isset($responseJson['error'])) {
+  print 'Oops';
+}
+else {
+  print $response;
+}
 ?>
