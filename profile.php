8ef36b51ef29d76f6400eaa7c1fc77af9b977795
holly <holly@localhost.localdomain>
2019-05-23 07:17:34 -0400

    building login

--- profile.php
+++ profile.php
@@ -1,4 +1,5 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
 $response = postJson('sessionCheck', '1.0', json_encode(array('token' => $_COOKIE['sessionToken'])));
 $responseJson = json_decode($response, true);
 if ($responseJson === null or isset($responseJson['error'])) {
