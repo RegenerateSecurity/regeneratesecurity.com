4dcdd535f97a0b439fb65461d54fc52f6391f653
holly <holly@localhost.localdomain>
2019-05-24 06:08:29 -0400

    building login

--- profile.php
+++ profile.php
@@ -2,10 +2,10 @@
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
 $response = postJson('sessionCheck', '1.0', json_encode(array('token' => $_COOKIE['sessionToken'])));
 $responseJson = json_decode($response, true);
-if ($responseJson === null or isset($responseJson['error'])) {
-  print 'Oops';
+if (!isset($responseJson['result']) or $responseJson['result'] != 'valid') {
+  softRedirect('/login/')
 }
 else {
-  print $response;
+  print 'Logged in';
 }
 ?>
