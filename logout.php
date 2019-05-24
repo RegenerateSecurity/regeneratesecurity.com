3802bd0713e530809c52702eb2cff64b3c635f2d
holly <holly@localhost.localdomain>
2019-05-24 09:02:06 -0400

    building login

--- logout.php
+++ logout.php
@@ -4,8 +4,11 @@ include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
 if (isset($_COOKIE['sessionToken'])) {
   $response = postJson('logoutSession', '1.0', json_encode(array('token' => $_SESSION['sessionToken'])));
   $responseJson = json_decode($response, true);
-  if ($responseJson === null or isset($responseJson['error'])) {
+  if ($responseJson === null) {
     print 'API Error'; // Handle me Daddy
+  }
+  else if (isset($responseJson['error'])) {
+    print '[!] ' . $responseJson['error'] . PHP_EOL;
   }
   else if (isset($responseJson['result'])) { // TODO: consider successful and invalid-logout separately
    softRedirect('/');
