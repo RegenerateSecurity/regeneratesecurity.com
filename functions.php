232be037bd103c53179aeb6afc95a062b1a9eb6d
holly <holly@localhost.localdomain>
2019-05-24 07:14:45 -0400

    building login

--- functions.php
+++ functions.php
@@ -2,26 +2,28 @@
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/hmac.php';
 
 // A function to execute a prepared query against an array of vars
-function execPrepare($mysqli, $query, $param) {
-  $stmt = $mysqli->prepare($query);
-  call_user_func_array(array($stmt, 'bind_param'), $param);
-  $stmt->execute();
-  $result = $stmt->get_result();
-  return $result;
-}
+//function execPrepare($mysqli, $query, $param) {
+//  $stmt = $mysqli->prepare($query);
+//  call_user_func_array(array($stmt, 'bind_param'), $param);
+//  $stmt->execute();
+//  $result = $stmt->get_result();
+//  return $result;
+//}
 // A function to execute a prepared query and return the number of rows
-function numPrepare($mysqli, $query, $param) {
-  $stmt = $mysqli->prepare($query);
-  call_user_func_array(array($stmt, 'bind_param'), $param);
-  $stmt->execute();
-  $stmt->store_result();
-  $result = $stmt->get_result();
-  return $stmt->num_rows;
-}
+//function numPrepare($mysqli, $query, $param) {
+//  $stmt = $mysqli->prepare($query);
+//  call_user_func_array(array($stmt, 'bind_param'), $param);
+//  $stmt->execute();
+//  $stmt->store_result();
+//  $result = $stmt->get_result();
+//  return $stmt->num_rows;
+//}
+
 function clean($input) {
   $input = htmlspecialchars($input);	// Abstracted to make custom filtration possible later
   return $input;
 }
+
 function softRedirect($target) {	// "Soft" because 302, non-permanent
   $target = str_replace("<", "&gt;", $target);
   $target = str_replace(">", "&lt;", $target);
@@ -30,6 +32,7 @@ function softRedirect($target) {	// "Soft" because 302, non-permanent
   header('Location: ' . $target);
   exit();
 }
+
 function display($input) {
   $input = str_replace("<", "&lt;", $input);
   $input = str_replace(">", "&gt;", $input);
