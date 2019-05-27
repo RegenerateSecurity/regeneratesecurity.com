775c27b3b07f3084734365d5485a2ae6da44da23
holly <holly@localhost.localdomain>
2019-05-27 15:42:33 -0400

    building vuln db

--- admin/articles.php
+++ admin/articles.php
@@ -2,7 +2,7 @@
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
 
 if (!isset($session['privs']) or $session['privs'] !== 2) {
-  softRedirect(/login/);
+  softRedirect('/login/');
 }
 
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
