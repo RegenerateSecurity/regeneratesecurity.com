ece597dd929bf6b9a716c83e068472263a1f90db
holly <holly@localhost.localdomain>
2019-05-27 09:18:11 -0400

    building login

--- navbar.php
+++ navbar.php
@@ -1,9 +1,9 @@
 <div class="title-bar"><div class="title"><span class="title-left">Regenerate</span><span class="title-right">Security</span></div></div>
 <div class="nav-bar">
 <ul>
-<li>News</li>
-<li>Dashboard</li>
-<li>Status</li>
-<li>VulnDB</li>
+<li><a href="/articles/">Articles<a></li>
+<li><a href="/dashboard/">Dashboard</a></li>
+<li><a href="/status/">Status</a></li>
+<li><a href="/vulndb">VulnDB</a></li>
 </ul>
 </div>
