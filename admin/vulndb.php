4579d739d92beaff742c5f8e8fd18b10002c1bdd
holly <holly@localhost.localdomain>
2019-05-27 15:41:36 -0400

    building vuln db

--- admin/vulndb.php
+++ admin/vulndb.php
@@ -1,5 +1,10 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
+
+if (!isset($session['privs']) or $session['privs'] !== 2) {
+  softRedirect(/login/);
+}
+
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
