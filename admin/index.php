e250c81d67a7494dea91079dc2aba2814a32d32d
holly <holly@localhost.localdomain>
2019-05-27 15:21:16 -0400

    building vuln db

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/functions.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/headers.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/head.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/topbar.php';
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/navbar.php';
+?>
+<div class="content-wrapper">
+<div class="center-text">
+<h1 class="black-text">Admin</h1>
+<p>Description here</p>
+</div>
+<div class="content-block">
+Content here
+</div>
+</div>
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] .  '/footer.php';
+?>
