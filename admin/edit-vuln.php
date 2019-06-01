8ea7801bcbe2bb11f37961718f7b9ec2fbffad58
holly <holly@localhost.localdomain>
2019-06-01 14:18:13 -0400

    building vuln db

--- admin/edit-vuln.php
+++ admin/edit-vuln.php
@@ -39,6 +39,7 @@ $cvssv3vector = clean($row['CVSSv3Vector']);
 <div class="content-wrapper">
 <div class="content-block">
 <form method="POST" action="/admin/update-vuln/">
+
 <input class="login-input dim-input" name="id" value="<?php print $id; ?>" readonly>
 <input class="login-input" name="name" value="<?php print $name; ?>" placeholder="Name">
 <input class="login-input" name="slug" value="<?php print $slug; ?>" placeholder="Slug">
@@ -46,10 +47,10 @@ $cvssv3vector = clean($row['CVSSv3Vector']);
 <input class="login-input" name="description" value="<?php print $description; ?>" placeholder="Description">
 <input class="login-input" name="remediation" value="<?php print $remediation; ?>" placeholder="Remediation">
 <input class="login-input" name="analysis" value="<?php print $analysis; ?>" placeholder="Analysis">
-<input class="login-input" name="cvssv3" value="<?php print $cvssv3; ?>" placeholder="CVSSv3 Base">
-<input class="login-input" name="cvssv3vector" value="<?php print $cvssv3vector; ?>" placeholder="CVSSv3Vector">
-<input class="login-input" name="cvssv2" value="<?php print $cvssv2; ?>" placeholder="CVSSv2 Base">
-<input class="login-input" name="cvssv2vector" value="<?php print $cvssv2vector; ?>" placeholder="CVSSv2 Vector">
+<input class="login-input" name="CVSSv3Base" value="<?php print $cvssv3; ?>" placeholder="CVSSv3 Base">
+<input class="login-input" name="CVSSv3Vector" value="<?php print $cvssv3vector; ?>" placeholder="CVSSv3Vector">
+<input class="login-input" name="CVSSv2Base" value="<?php print $cvssv2; ?>" placeholder="CVSSv2 Base">
+<input class="login-input" name="CVSSv2Vector" value="<?php print $cvssv2vector; ?>" placeholder="CVSSv2 Vector">
 <input class="admin-button" type="submit">
 </form>
 </div>
